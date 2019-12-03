e81bf0be7e4cf94e6ac47fc268b8adfad94e072d
phoenixbird <chaojilsp2012@163.com>
2019-12-03 20:39:17 +0800

    新增收货地址及Vue,三级联动

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Auth::routes(['verify'=>true]);
 //用户地址列表路由
 Route::group(['middleware'=>['auth','verified']],function (){
     Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
 });
 
