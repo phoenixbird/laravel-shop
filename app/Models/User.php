b595bb430539df6d53dc6b8ae1289707502cb9f2
phoenixbird <chaojilsp2012@163.com>
2019-12-06 11:17:23 +0800

    把商品加入到购物车中

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,10 @@ class User extends Authenticatable implements MustVerifyEmail
             ->withTimestamps()
             ->orderBy('user_favorite_products.created_at','desc');
     }
+
+    //用户与购物车关联
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
