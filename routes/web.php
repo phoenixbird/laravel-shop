b8a06a233e3b1151edf3b9b646b95cdc90390a3e
phoenixbird <chaojilsp2012@163.com>
2019-12-13 15:45:42 +0800

    手误字段写错导致bug,用户界面购物车增加优惠券

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,9 @@ Route::group(['middleware'=>['auth','verified']],function (){
 
     //退款申请
     Route::post('orders/{order}/apply_refund','OrdersController@applyRefund')->name('orders.apply_refund');
+
+    //优惠券校验
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 Route::redirect('/', '/products')->name('root');
